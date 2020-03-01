b939e64c35113fbed15dc652f06e13a47031027f
Zxilly <zhouxinyu1001@gmail.com>
2020-03-01 12:44:53 +0800

    separate

--- index.php
+++ index.php
@@ -1,14 +1,5 @@
 <?php
-$host1 = dns_get_record("onedrive.live.com",DNS_A);
-$host2 = dns_get_record("skyapi.onedrive.live.com",DNS_A);
-$host3 = dns_get_record("api.onedrive.live.com",DNS_A);
-$host4 = dns_get_record("d.docs.live.net",DNS_A);
-$host5 = dns_get_record("contentsync.onenote.com",DNS_A);
-$host6 = dns_get_record("hierarchyapi.onenote.com",DNS_A);
-$host7 = dns_get_record("ocws.officeapps.live.com",DNS_A);
-$host8 = dns_get_record("www.onenote.com",DNS_A);
-$host9 = dns_get_record("config.edge.skype.com",DNS_A);
-$host10 = dns_get_record("roaming.officeapps.live.com",DNS_A);
+require("dns_get.php");
 //print_r($host1);
 /*
 echo "onedrive.live.com ".$host1[0]['ip'];
@@ -29,4 +20,5 @@ echo <<<DNS
 {$host8[0]['ip']} www.onenote.com <br>
 {$host9[0]['ip']} config.edge.skype.com <br>
 {$host10[0]['ip']} roaming.officeapps.live.com <br>
+{$host11[0]['ip']} pagecontentsync.onenote.com <br>
 DNS;
--- txt.php
+++ txt.php
@@ -1,23 +1,6 @@
 <?php
-$host1 = dns_get_record("onedrive.live.com",DNS_A);
-$host2 = dns_get_record("skyapi.onedrive.live.com",DNS_A);
-$host3 = dns_get_record("api.onedrive.live.com",DNS_A);
-$host4 = dns_get_record("d.docs.live.net",DNS_A);
-$host5 = dns_get_record("contentsync.onenote.com",DNS_A);
-$host6 = dns_get_record("hierarchyapi.onenote.com",DNS_A);
-$host7 = dns_get_record("ocws.officeapps.live.com",DNS_A);
-$host8 = dns_get_record("www.onenote.com",DNS_A);
-$host9 = dns_get_record("config.edge.skype.com",DNS_A);
-$host10 = dns_get_record("roaming.officeapps.live.com",DNS_A);
-//print_r($host1);
-/*
-echo "onedrive.live.com ".$host1[0]['ip'];
-echo "<br>";
-//print_r($host2);
-echo "skyapi.onedrive.live.com ".$host2[0]['ip'];
-echo "<br>";
-echo "api.onedrive.live.com ".$host3[0]['ip'];
-*/
+require("dns_get.php");
+
 echo <<<DNS
 {$host1[0]['ip']} onedrive.live.com
 {$host2[0]['ip']} skyapi.onedrive.live.com
@@ -29,4 +12,5 @@ echo <<<DNS
 {$host8[0]['ip']} www.onenote.com
 {$host9[0]['ip']} config.edge.skype.com
 {$host10[0]['ip']} roaming.officeapps.live.com
+{$host11[0]['ip']} pagecontentsync.onenote.com
 DNS;
