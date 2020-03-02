7ad50500eacb015e994edf958fb16746efaf1374
Zxilly <zhouxinyu1001@gmail.com>
2020-03-02 12:52:10 +0800

    change cache time && format code

--- index.php
+++ index.php
@@ -5,53 +5,48 @@ require "dns_get.php";
 
 function get_cache_time()
 {
-    $file = fopen("time.pid","r+");
-    $time_last = fread($file,filesize("time.pid"));
+    $file = fopen("time.pid", "r+");
+    $time_last = fread($file, filesize("time.pid"));
     $time_now = mktime();
     fclose($file);
-    return $time_now-$time_last;
+    return $time_now - $time_last;
 }
 
 function refresh_time()
 {
-    $file = fopen("time.pid","w+");
-    fwrite($file,mktime());
+    $file = fopen("time.pid", "w+");
+    fwrite($file, mktime());
     fclose($file);
 }
 
 function refresh_dns_cache()
 {
     global $domain_list;
-    $file = fopen("dns.cache","w+") or die("Unable to open file!");
-    fwrite($file,"####### Onenote Hosts Start #######"."\n");
-    foreach ($domain_list as $domain)
-    {
+    $file = fopen("dns.cache", "w+") or die("Unable to open file!");
+    fwrite($file, "####### Onenote Hosts Start #######" . "\n");
+    foreach ($domain_list as $domain) {
         //echo dns_get_record($domain,DNS_A)[0]['ip']." ".$domain."\n";
         //fwrite($file,"233\n");
-        fwrite($file,dns_get_record($domain,DNS_A)[0]['ip']." ".$domain."\n");
+        fwrite($file, dns_get_record($domain, DNS_A)[0]['ip'] . " " . $domain . "\n");
     }
-    fwrite($file,"####### Onenote Hosts End #######"."\n");
+    fwrite($file, "####### Onenote Hosts End #######" . "\n");
     fclose($file);
     refresh_time();
 }
 
 function read_dns_cache()
 {
-    $file = fopen("dns.cache","r+");
-    while(!feof($file))
-    {
-        echo fgets($file)."<br>";
+    $file = fopen("dns.cache", "r+");
+    while (!feof($file)) {
+        echo fgets($file) . "<br>";
     }
     fclose($file);
 }
 
-if(get_cache_time()>3600)
-{
+if (get_cache_time() > 60000) {
     refresh_dns_cache();
     read_dns_cache();
-}
-else
-{
+} else {
     read_dns_cache();
 }
 
