f93eacec0b70acbf5b92c57762405a9eac6e8ada
GardenParty <zxilly@outlook.com>
2022-05-02 16:51:01 +0000

    refactor: remove unused code

--- dns_get.php
+++ dns_get.php
@@ -1,17 +1,4 @@
 <?php
-/*
-$host1 = dns_get_record("onedrive.live.com",DNS_A);
-$host2 = dns_get_record("skyapi.onedrive.live.com",DNS_A);
-$host3 = dns_get_record("api.onedrive.live.com",DNS_A);
-$host4 = dns_get_record("d.docs.live.net",DNS_A);
-$host5 = dns_get_record("contentsync.onenote.com",DNS_A);
-$host6 = dns_get_record("hierarchyapi.onenote.com",DNS_A);
-$host7 = dns_get_record("ocws.officeapps.live.com",DNS_A);
-$host8 = dns_get_record("www.onenote.com",DNS_A);
-$host9 = dns_get_record("config.edge.skype.com",DNS_A);
-$host10 = dns_get_record("roaming.officeapps.live.com",DNS_A);
-$host11 = dns_get_record("pagecontentsync.onenote.com",DNS_A);
-*/
 $domain_list = array(
     "contentstorage.osi.office.net",
     "onedrive.live.com",
--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php
 require "dns_get.php";
-//include "dns_get.php";
-
 
 function get_cache_time()
 {
@@ -49,43 +47,3 @@ if (get_cache_time() > 60000) {
 } else {
     read_dns_cache();
 }
-
-
-
-
-
-
-/*
-echo "####### Onenote Hosts Start #######"."<br>";
-
-foreach ($domain_list as $domain)
-{
-    echo dns_get_record($domain,DNS_A)[0]['ip']." ".$domain."<br>";
-}
-
-echo "####### Onenote Hosts End #######"."<br>";
-*/
-//print_r($host1);
-/*
-echo "onedrive.live.com ".$host1[0]['ip'];
-echo "<br>";
-//print_r($host2);
-echo "skyapi.onedrive.live.com ".$host2[0]['ip'];
-echo "<br>";
-echo "api.onedrive.live.com ".$host3[0]['ip'];
-*/
-/*
-echo <<<DNS
-{$host1[0]['ip']} onedrive.live.com <br>
-{$host2[0]['ip']} skyapi.onedrive.live.com <br>
-{$host3[0]['ip']} api.onedrive.live.com <br>
-{$host4[0]['ip']} d.docs.live.net <br>
-{$host5[0]['ip']} contentsync.onenote.com <br>
-{$host6[0]['ip']} hierarchyapi.onenote.com <br>
-{$host7[0]['ip']} ocws.officeapps.live.com <br>
-{$host8[0]['ip']} www.onenote.com <br>
-{$host9[0]['ip']} config.edge.skype.com <br>
-{$host10[0]['ip']} roaming.officeapps.live.com <br>
-{$host11[0]['ip']} pagecontentsync.onenote.com <br>
-DNS;*/
-
